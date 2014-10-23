b09d95ad7c9f922e330233748e8d217b961d3025
sejong <umlstudy@gmail.com>
2014-10-23 10:30:28 +0900

    connection creation cancel by right mouse button.

--- src/asia/sejong/freedrawing/editor/FDConnectionCreationTool.java
+++ src/asia/sejong/freedrawing/editor/FDConnectionCreationTool.java
@@ -1,12 +1,17 @@
 package asia.sejong.freedrawing.editor;
 
 import org.eclipse.gef.tools.ConnectionCreationTool;
+import org.eclipse.gef.ui.palette.PaletteViewer;
 
 public class FDConnectionCreationTool extends ConnectionCreationTool {
 
 	protected boolean handleButtonDown(int button) {
-		
 		System.out.println("llllllllllllllllll");
-		return false;
+		
+		if ( button == 3 ) {
+			PaletteViewer paletteViewer = getDomain().getPaletteViewer();
+			paletteViewer.setActiveTool(paletteViewer.getPaletteRoot().getDefaultEntry());
+		}
+		return super.handleButtonDown(button);
 	}
 }
